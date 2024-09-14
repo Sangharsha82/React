05af9e9770516d90cdd54a249734db3a2692f0da
Sangharsha <sangamtyata3@gmail.com>
2024-09-14 20:24:14 +0545

    signup

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -7,28 +7,41 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+       <div class="h-screen flex justify-center items-center bg-gray-100">
+      <form
+        action="/"
+        method="get"
+        class="inline-block justify-center items-center max-w-md w-full bg-white rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <h2 class="text-3xl font-bold mb-4">Login</h2>
+        <label for="cardNumber" class="block mb-2">Card Number:</label>
+        <input
+          type="text"
+          id="cardNumber"
+          name="cardNumber"
+          class="w-full px-4 py-2 border border-gray-300 rounded"
+        />
+        <br /><br />
+        <label for="pin" class="block mb-2">PIN:</label>
+        <input
+          type="password"
+          id="pin"
+          name="pin"
+          class="w-full px-4 py-2 border border-gray-300 rounded"
+        />
+        <br /><br />
+        <div class="place-items-center">
+          <input
+            type="submit"
+            value="Login"
+            class="bg-carribean-primary hover:bg-green-950 text-white font-bold py-2 px-4 rounded"
+          />
+        </div>
+      </form>
+      <div></div>
+    </div>
+    </div>
   )
 }
 
